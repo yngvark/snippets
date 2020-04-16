7ad9ad57c1e095e505f05e2a543d52cda8f98970
Yngvar Kristiansen <yngkri@knowit.no>
2020-04-16 09:03:45 +0200

    Add Notion

--- pc-init/src/main/kotlin/com/yngvark/pc_init/process/WebPagesProcess.kt
+++ pc-init/src/main/kotlin/com/yngvark/pc_init/process/WebPagesProcess.kt
@@ -15,7 +15,11 @@ class WebPagesProcess(private val robot: RobotHelper) {
         robot.type("https://mail.google.com/mail/u/2/#inbox").enter()
 
         robot.pressAndRelease(KeyEvent.VK_CONTROL, KeyEvent.VK_T).sleep(50)
-        robot.type("https://webmail.oslo.kommune.no").enter().sleep(1000)
+        robot.type("https://www.notion.so").enter()
+
+        robot.pressAndRelease(KeyEvent.VK_CONTROL, KeyEvent.VK_T).sleep(50)
+        // Super cool URL that doesn't prompt for username and password via a stupid alert box, when Outlook is stuck in that state
+        robot.type("https://webmail.oslo.kommune.no/owa/auth/logon.aspx?replaceCurrent=1&url=http%3a%2f%2fwebmail.oslo.kommune.no%2fowa").enter().sleep(1000)
         robot.type(password).enter().sleep(300)
     }
 
