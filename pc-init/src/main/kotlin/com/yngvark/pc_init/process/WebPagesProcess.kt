0cab72a616c630dfc888eb973de376f5a3c0292d
Yngvar Kristiansen <yngkri@knowit.no>
2020-04-28 08:12:15 +0200

    Enter outlook password with 1password dropdown

--- pc-init/src/main/kotlin/com/yngvark/pc_init/process/WebPagesProcess.kt
+++ pc-init/src/main/kotlin/com/yngvark/pc_init/process/WebPagesProcess.kt
@@ -8,8 +8,6 @@ class WebPagesProcess(private val robot: RobotHelper) {
     fun run() {
         println("Process: ${javaClass.simpleName}")
 
-        val password = secretGetter.getPasswordFromWebsite("Oslo Kommune AD byr281592")
-
         robot.pressAndRelease(KeyEvent.VK_CONTROL, KeyEvent.VK_T).sleep(50)
         robot.type("https://mail.google.com/mail/u/0/#inbox").enter()
 
@@ -21,8 +19,8 @@ class WebPagesProcess(private val robot: RobotHelper) {
 
         robot.pressAndRelease(KeyEvent.VK_CONTROL, KeyEvent.VK_T).sleep(50)
         // Super cool URL that doesn't prompt for username and password via a stupid alert box, when Outlook is stuck in that state
-        robot.type("https://webmail.oslo.kommune.no/owa/auth/logon.aspx?replaceCurrent=1&url=http%3a%2f%2fwebmail.oslo.kommune.no%2fowa").enter().sleep(3000)
-        robot.type(password).enter().sleep(300)
+        robot.type("https://webmail.oslo.kommune.no/owa/auth/logon.aspx?replaceCurrent=1&url=http%3a%2f%2fwebmail.oslo.kommune.no%2fowa").enter().sleep(2000)
+        robot.pressAndRelease(KeyEvent.VK_DOWN).enter().enter() // Uses 1password login box
     }
 
 }
