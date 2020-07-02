48cb5b6b3854d7c841fe419fadf3565aa27f918e
Yngvar Kristiansen <yngkri@knowit.no>
2020-07-02 08:49:26 +0200

    Add api hosts to header printer

--- docker/header-printer/server.js
+++ docker/header-printer/server.js
@@ -2,10 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+function logz(msg) {
+    log = {
+        message: msg
+    }
+    console.log(JSON.stringify(log))
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
-    console.log('Headers:')
-    console.log(JSON.stringify(req.headers));
+    logz('Headers:')
+
+    headerLog = {
+        ...req.headers,
+        ...{ message: 'print headers' }
+    }
+    //console.log(req.headers, null, 2);
+    console.log(JSON.stringify(headerLog));
 })
 
 
